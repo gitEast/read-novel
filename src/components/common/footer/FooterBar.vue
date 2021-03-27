20901d94685555b4b756cb2e8cc8edf5eb2202dd
EastWind <linxiayoudongfeng@gmail.com>
2021-03-27 21:38:28 +0800

    footer完成

---
+++ src/components/common/footer/FooterBar.vue
@@ -0,0 +1,74 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-26 21:00:43
+ * @LastEditTime: 2021-03-27 21:37:12
+ * @LastEditors: Please set LastEditors
+ * @Description: 尾部
+ * @FilePath: \read-novel\src\components\common\footer\Footer.vue
+-->
+<template>
+  <div class="footer">
+    <div class="links">
+      <a href="#">首页</a>
+      <a href="#">博客</a>
+      <a href="https://github.com/gitEast/read-novel" target="_blank">github</a>
+    </div>
+    <div class="email">
+      <!-- <img src="~assets/img/footer/email.png" alt="email icon"> -->
+    </div>
+    <div class="copyright">&copy;&nbsp;&middot;&nbsp;East&nbsp;Wind&nbsp;&middot;&nbsp;2021</div>
+  </div>
+  
+</template>
+<script>
+export default {
+  name: 'FooterBar'
+}
+</script>
+<style scoped>
+.footer {
+  width: 1100px;
+  margin: 100px auto 50px;
+  text-align: center;
+  color: #636e72;
+}
+.links {
+  width: 40%;
+  margin: 0 auto;
+  padding: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid #636e72;
+  font-size: 1.3rem;
+}
+.links a {
+  text-decoration: none;
+  color: #636e72;
+}
+a:hover {
+  color: #444;
+}
+.email {
+  margin: 10px auto;
+  background-image: url('~assets/img/footer/email.png');
+  background-size: contain;
+  width: 40px;
+  height: 40px;
+  position: relative;
+  left: -12px;
+  cursor: pointer;
+}
+.email:hover {
+  background-image: url('~assets/img/footer/email-02.png');
+}
+/* .email img {
+  width: 40px;
+  position: relative;
+  left: -11px;
+  cursor: pointer;
+} */
+.copyright {
+  cursor: default;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,7 +4,7 @@
  * @Description: 主界面
  * @Date: 2021-02-28 22:55:50
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-03-26 19:09:31
+ * @LastEditTime: 2021-03-26 21:30:10
 -->
 <template>
   <div id="home">
@@ -12,6 +12,7 @@
     <search-bar />
     <slide-show />
     <finished-book-card />
+    <footer-bar />
   </div>
 </template>
 <script>
@@ -19,6 +20,7 @@ import FinishedNavBar from 'components/content/finishedNavbar/FinishedNavBar'
 import SearchBar from 'components/common/searchbar/SearchBar'
 import SlideShow from 'components/common/slideshow/SlideShow'
 import FinishedBookCard from 'components/content/finishedBookCard/FinishedBookCard'
+import FooterBar from 'components/common/footer/FooterBar'
 
 export default {
   name: 'Home',
@@ -26,7 +28,8 @@ export default {
     FinishedNavBar,
     SearchBar,
     SlideShow,
-    FinishedBookCard
+    FinishedBookCard,
+    FooterBar
   }
 }
 </script>
